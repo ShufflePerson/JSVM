c63aa773aaada4f41be5a633a9923a03bf10e008
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2024-01-26 16:33:42 +0200

    Refactored tests

--- src/Tests.ts
+++ src/Tests.ts
@@ -10,26 +10,27 @@ function testFailed(data: string) {
     console.log('\x1b[31m%s\x1b[0m', data);  
 }
 
-let load_str_parse = new Parser("", "./vm/load_str");
-load_str_parse.parse();
-load_str_parse.dumpToFile("./dumps/vm_load_str.dump")
+function checkRegister(cpu: CPU, cpuName: string, registerName: string, register: TRegisters, expectedValue: string | number) {
+    if (cpu.getRegister(register) == expectedValue) {
+        testPassed(`[${cpuName}] [${registerName}] [✓]`)
+    } else {
+        testFailed(`[${cpuName}] [${registerName}] [X] Expected: "${expectedValue}" but got "${cpu.getRegister(register)}"`)
+    }
+}
 
-let load_num_parse = new Parser("", "./vm/load_num");
-load_num_parse.parse();
-load_num_parse.dumpToFile("./dumps/vm_load_num.dump")
+function createParseDump(name: string): Parser {
+    let parser = new Parser("", "./vm/" + name);
+    parser.parse();
+    parser.dumpToFile("./dumps/vm_" + name + ".dump")
+    return parser
+}
 
-let variables_parse = new Parser("", "./vm/variables");
-variables_parse.parse();
-variables_parse.dumpToFile("./dumps/vm_variables.dump")
-
-let math_parse = new Parser("", "./vm/math");
-math_parse.parse();
-math_parse.dumpToFile("./dumps/vm_math.dump")
-
-
-let simple_jump_parse = new Parser("", "./vm/simple_jump");
-simple_jump_parse.parse();
-simple_jump_parse.dumpToFile("./dumps/vm_simple_jump.dump")
+let load_str_parse = createParseDump("load_str");
+let load_num_parse = createParseDump("load_num");
+let variables_parse = createParseDump("variables");
+let math_parse = createParseDump("math");
+let simple_jump_parse = createParseDump("simple_jump");
+let cond_jumps_parse = createParseDump("cond_jumps");
 
 
 console.log("       =========VM OUTPUTS=========")
@@ -38,21 +39,33 @@ let numCPU          = new CPU(load_num_parse.getByteCode(), false);
 let variablesCPU    = new CPU(variables_parse.getByteCode(), false);
 let mathCPU         = new CPU(math_parse.getByteCode(), false);
 let simpleJumpCPU   = new CPU(simple_jump_parse.getByteCode(), false);
+let condJumpsCPU    = new CPU(cond_jumps_parse.getByteCode(), false);
+
 console.log("       =========VM OUTPUTS=========")
 
 
-if (strCPU          .getRegister              (TRegisters.str1) == "console.log")             testPassed("[strCPU]        [✓] STR1 Register Passed");                          else testFailed("[strCPU]         [X] STR1 Register Failed");
-if (strCPU          .getRegister              (TRegisters.str2) == "Hello World!")            testPassed("[strCPU]        [✓] STR2 Register Passed");                          else testFailed("[strCPU]         [X] STR2 Register Failed");
-if (strCPU          .getRegister              (TRegisters.str3) == "I am a str3")             testPassed("[strCPU]        [✓] STR3 Register Passed");                          else testFailed("[strCPU]         [X] STR3 Register Failed");
-if (numCPU          .getRegister              (TRegisters.num1) == 69)                        testPassed("[numCPU]        [✓] NUM1 Register Passed");                          else testFailed("[numCPU]         [X] NUM1 Register Failed");
-if (numCPU          .getRegister              (TRegisters.num2) == 420)                       testPassed("[numCPU]        [✓] NUM2 Register Passed");                          else testFailed("[numCPU]         [X] NUM2 Register Failed");
-if (numCPU          .getRegister              (TRegisters.num3) == 1337)                      testPassed("[numCPU]        [✓] NUM3 Register Passed");                          else testFailed("[numCPU]         [X] NUM3 Register Failed");
-if (mathCPU         .getRegister              (TRegisters.num1) == 2)                        testPassed("[mathCPU]       [✓] NUM1 Register Passed");                          else testFailed("[mathCPU]        [X] NUM1 Register Failed");
-if (mathCPU         .getRegister              (TRegisters.num2) == 8)                        testPassed("[mathCPU]       [✓] NUM2 Register Passed");                          else testFailed("[mathCPU]        [X] NUM2 Register Failed");
-if (mathCPU         .getRegister              (TRegisters.num3) == 15)                       testPassed("[mathCPU]       [✓] NUM3 Register Passed");                          else testFailed("[mathCPU]        [X] NUM3 Register Failed");
-if (variablesCPU    .getRegister              (TRegisters.num1) == 1337)                testPassed("[variablesCPU]  [✓] VAR1 Register Passed");                    else testFailed("[variablesCPU]   [X] Variables1 Register Failed");
-if (variablesCPU    .getRegister              (TRegisters.str1) == "console.debug")     testPassed("[variablesCPU]  [✓] VAR2 Register Passed");                    else testFailed("[variablesCPU]   [X] Variables2 Register Failed");
-if (variablesCPU    .getRegister              (TRegisters.str2) == "Hello World. I am a very long variable with a lot of text haha hello") testPassed("[variablesCPU]  [✓] VAR3 Register Passed");              else testFailed("[variablesCPU] [X] Variables3 Register Failed");
-if (simpleJumpCPU   .getRegister              (TRegisters.str1) == "console.debug")     testPassed("[simpleJumpCPU] [✓] STR1 Register Passed");                    else testFailed("[simpleJumpCPU]   [X] Str1 Register Failed");
-if (simpleJumpCPU   .getRegister              (TRegisters.str2) == "I got loaded after the Jmp")     testPassed("[simpleJumpCPU] [✓] STR2 Register Passed");                    else testFailed("[simpleJumpCPU]   [X] Str2 Register Failed");
-if (simpleJumpCPU   .getRegister              (TRegisters.str3) == EMPTY_STRING)     testPassed("[simpleJumpCPU] [✓] STR3 Register Passed");                    else testFailed("[simpleJumpCPU]   [X] Str3 Register Failed");
+
+
+checkRegister(strCPU, "strCPU", "str1", TRegisters.str1, "console.log");
+checkRegister(strCPU, "strCPU", "str2", TRegisters.str2, "Hello World!");
+checkRegister(strCPU, "strCPU", "str3", TRegisters.str3, "I am a str3");
+
+checkRegister(numCPU, "numCPU", "num1", TRegisters.num1, 69);
+checkRegister(numCPU, "numCPU", "num2", TRegisters.num2, 420);
+checkRegister(numCPU, "numCPU", "num3", TRegisters.num3, 1337);
+
+checkRegister(mathCPU, "mathCPU", "num1", TRegisters.num1, 2);
+checkRegister(mathCPU, "mathCPU", "num2", TRegisters.num2, 8);
+checkRegister(mathCPU, "mathCPU", "num3", TRegisters.num3, 15);
+
+checkRegister(variablesCPU, "variablesCPU", "num1", TRegisters.num1, 1337);
+checkRegister(variablesCPU, "variablesCPU", "str1", TRegisters.str1, "console.debug");
+checkRegister(variablesCPU, "variablesCPU", "str3", TRegisters.str2, "Hello World. I am a very long variable with a lot of text haha hello");
+
+checkRegister(simpleJumpCPU, "simpleJumpCPU", "str1", TRegisters.str1, "console.debug");
+checkRegister(simpleJumpCPU, "simpleJumpCPU", "str2", TRegisters.str2, "I got loaded after the Jmp");
+checkRegister(simpleJumpCPU, "simpleJumpCPU", "str3", TRegisters.str3, EMPTY_STRING);
+
+checkRegister(condJumpsCPU, "simpleJumpCPU", "str1", TRegisters.str1, "console.debug");
+checkRegister(condJumpsCPU, "simpleJumpCPU", "num1", TRegisters.num1, 10);
+checkRegister(condJumpsCPU, "simpleJumpCPU", "num2", TRegisters.num1, 10);
